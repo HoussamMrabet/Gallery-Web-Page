f28080d84f62e1a438fbf04d0b20109c1c76c7d5
HoussamMrabet <dodslim100@gmail.com>
2021-01-13 01:21:42 +0100

    filtring by the search keyword

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const auth = "563492ad6f91700001000001987f90fb8f154861a4b5bd9ee26d0635";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-btn");
+const form = document.querySelector(".search-form");
 let seachValue;
 
-async function curatedPhotos() {
-    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=1",{
+// Event
+searchInput.addEventListener('input', updateInput);
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+    searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+    const dataFetch = await fetch(url, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -13,14 +24,38 @@ async function curatedPhotos() {
         }
     });
     const data = await dataFetch.json();
+    return data;
+}
+
+function generatePictures(data) {
     data.photos.forEach(photo => {
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>
+        galleryImg.innerHTML = `
+            <div class="gallery-info">
+                <p>${photo.photographer}</p>
+                <a href=${photo.src.original}>Dowload</a>
+            </div>
+            <img src=${photo.src.large}></img>
         `;
         gallery.appendChild(galleryImg);
     });
+}
+
+async function curatedPhotos() {
+    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
+    generatePictures(data);
+}
+
+async function searchPhotos(query) {
+    clear();
+    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`);
+    generatePictures(data);
+}
+
+function clear() {
+    gallery.innerHTML = "";
+    searchInput.value = "";
 }
 
 curatedPhotos();
